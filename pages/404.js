b3fb882486f4a082ced1b6b065deeaeb3c3d1380
Sherry90 <hhlsdkm@gmail.com>
2024-06-01 20:28:41 +0900

    second commit section 12

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-  return (
-    <dev>
-      <h1>Page Not Found!</h1>
-    </dev>
-  );
-};
-
-export default NotFoundPage;
--- pages/blog/[...slug].js
+++
@@ -1,9 +0,0 @@
-const BlogPostPage = () => {
-  return (
-    <div>
-      <h1>The Blog Posts</h1>
-    </div>
-  );
-}
-
-export default BlogPostPage;
