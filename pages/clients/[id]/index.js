0fd6d66938febdbdcc2416f2759c046c332d2709
Sherry90 <hhlsdkm@gmail.com>
2024-06-01 17:59:03 +0900

    feat: routing programming

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    router.push("/clients/max/projecta");
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
