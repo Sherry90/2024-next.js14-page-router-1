ecc5c2c81b91a51818460123813c8203fcae89af
Sherry90 <hhlsdkm@gmail.com>
2024-06-03 22:25:19 +0900

    feat: inject event-item props

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -5,8 +5,15 @@ const EventList = (props) => {
 
   return (
     <ul>
-      {items.map((item) => (
-        <EventItem key={items.id} />
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
       ))}
     </ul>
   );
