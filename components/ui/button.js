e7c38fae87f1cbfcd9dd7aef1c9df74aae230895
Sherry90 <hhlsdkm@gmail.com>
2024-06-04 22:34:57 +0900

    fix: format

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
-import classes from "./button.module.css"
+import classes from "./button.module.css";
 
 const Button = (props) => {
-  return <Link className={classes.btn} href={props.link}>{props.children}</Link>;
+  return (
+    <Link className={classes.btn} href={props.link}>
+      {props.children}
+    </Link>
+  );
 };
 
 export default Button;
