b3026d027d446f64cd7ba2b6e0d7ac313b23cf60
Sherry90 <hhlsdkm@gmail.com>
2024-06-04 22:38:59 +0900

    feat: use Button component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,4 @@
-import Link from "next/link";
-
+import Button from "../ui/button";
 import classes from "./event-item.module.css"
 
 const EventItem = (props) => {
@@ -27,7 +26,7 @@ const EventItem = (props) => {
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}> Explore Event</Link>
+          <Button link={exploreLink}> Explore Event</Button>
         </div>
       </div>
     </li>
