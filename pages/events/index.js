f32801b1ac7fe8dd5179402c857ccac2602c1c8a
Sherry90 <hhlsdkm@gmail.com>
2024-06-07 18:35:18 +0900

    feat: getAllEvents

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,14 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList items={events} />
     </div>
   );
-}
+};
 
-export default AllEventsPage
+export default AllEventsPage;
