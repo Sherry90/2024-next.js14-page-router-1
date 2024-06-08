c8884d53b3c030abce0cb5fd734389350ba4926b
Sherry90 <hhlsdkm@gmail.com>
2024-06-08 17:21:06 +0900

    feat: useRouter, valid check

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,26 @@
+import { useRouter } from "next/router";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+
+  if (!filteredData) {
+    return <p className="center">Loading</p>;
+  }
+
+  const numYear = +filteredData[0];
+  const numMonth = +filteredData[1];
+
+  if (isNaN(numYear) || isNaN(numMonth) || numMonth < 1 || numMonth > 12) {
+    return <p>Invalid filter.</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
     </div>
   );
-}
+};
 
-export default FilteredEventsPage
+export default FilteredEventsPage;
